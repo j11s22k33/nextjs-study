cc0c0b76833e1893220514e2e3924a5402d611c2
장성권 <j11s22k33@gmail.com>
2021-03-04 16:19:21 +0900

    1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/index.scss'
+// import '../styles/globals.css'
+import { useStateCallbackWrapper } from '../utils/common.tsx'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [, uTmp] = useStateCallbackWrapper(0)
+  function updateUI({ useLayoutEffect, useEffect }) {
+    uTmp({
+      setState: c => c + 1,
+      useLayoutEffect,
+      useEffect
+    })
+  }
+
+  return (
+    <>
+      <Component {...pageProps} updateUI={updateUI} />
+    </>
+  )
 }
 
 export default MyApp
---
+++ utils/common.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+
+/**
+ * ```
+ * // 예제
+ * const [cnt, updateCnt] = useStateCallbackWrapper(0)
+ *
+ * updateCnt({
+ *    setState: oldState => oldState+1,
+ *    useLayoutEffect: newState => console.log(newState),
+ *    useEffect: newState => console.log(newState)
+ * })
+ *
+ * ```
+ * @param initialState
+ * @returns [state, updateState]
+ */
+const $emptyFn = (state) => { };
+const useStateCallbackWrapper = <T extends unknown>(initialState?: T): [T, Function] => {
+  const [$state, $setState] = useState(initialState);
+  const $res = useRef({ effect: $emptyFn, layoutEffect: $emptyFn });
+
+  useLayoutEffect(() => {
+    $res.current.layoutEffect($state);
+  }, [$state]);
+
+  useEffect(() => {
+    $res.current.effect($state);
+  }, [$state]);
+
+  function $udateState({ setState, useLayoutEffect = $emptyFn, useEffect = $emptyFn }) {
+    $res.current.effect = useEffect;
+    $res.current.layoutEffect = useLayoutEffect;
+    $setState(setState);
+  }
+
+  return [$state, $udateState];
+}
+
+export { useStateCallbackWrapper }
