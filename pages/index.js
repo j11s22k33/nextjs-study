6aa217c8efd46a2bd82acd4d486828cc53ef3027
장성권 <j11s22k33@gmail.com>
2021-03-04 19:37:00 +0900

    1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-// import styles from '@/styles/Home.module.css'
+// import styles from "../styles/Home.module.css";
+import styles from "@/styles/Home.module.css";
+import { useStateCallbackWrapper } from "@/utils/common.tsx";
 
 export default function Home() {
   console.log("process.env => ", process.env);
