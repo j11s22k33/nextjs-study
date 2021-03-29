162a74ad6936046e3777dd4048527cd3fa2bd365
장성권 <j11s22k33@gmail.com>
2021-03-29 12:26:49 +0900

    1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 const $name = "[MyIndex]";
 
 export default function MyIndex({ updateUI }) {
+  const path = useRef([
+    "/ex/popup",
+    "/ex/css",
+    "/ex/image",
+    "/ex/preload",
+    "/ex/useContext",
+    "/ex/redux",
+  ])
   useEffect(() => {
     console.log($name, "component mount");
 
@@ -18,6 +27,9 @@ export default function MyIndex({ updateUI }) {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {
+        path.current.map((item, idx)=>(<><Link href={item}>{item}</Link><br /></>))
+      }
       <h1 className="red">Index</h1>
       <style jsx>{`
         $color-red: red;
